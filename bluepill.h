ff2c10bcef44f90f3fa6aa04567796a8cbb060ba
JZJisawesome <JZJisawesome123@hotmail.com>
2021-03-27 21:53:31 -0400

    Fix issues with certain boards being picky about accessing dma registers by halfwords

--- bluepill.h
+++ bluepill.h
@@ -64,6 +64,8 @@ typedef unsigned long long uintmax_t;
 
 typedef unsigned int* uintptr_t;
 
+typedef unsigned int size_t;
+
 #define bool _Bool
 #define true 1
 #define false 0
@@ -72,8 +74,13 @@ typedef unsigned int* uintptr_t;
 #ifdef NDEBUG
     #define assert(condition) ((void)0)
 #else//Spin forever if assertion fails (allows debugger to be used)
-    #define assert(condition) do {if (condition == 0) __hang();} while (0)
+    #define assert(condition) do {if ((condition) == 0) __hang();} while (0)
 #endif
+
+extern int memcmp(const void* lhs, const void* rhs, size_t count);//TODO
+extern void* memset(void* dest, int ch, size_t count);//TODO
+extern void* memcpy(void* dest, const void* src, size_t count);//TODO
+extern void* memmove(void* dest, const void* src, size_t count);//TODO
 
 /* Useful functions */
 extern void __delayInstructions(uint32_t numberOfInstructions);//21.1851ns per instruction avg
@@ -543,38 +550,38 @@ extern void __delayInstructions(uint32_t numberOfInstructions);//21.1851ns per i
 #define DMA_ISR (*(volatile uint32_t*)(0x40020000))
 #define DMA_IFCR (*(volatile uint32_t*)(0x40020004))
 
-#define DMA_CCR1 (*(volatile uint16_t*)(0x40020008))
-#define DMA_CNDTR1 (*(volatile uint16_t*)(0x4002000C))
+#define DMA_CCR1 (*(volatile uint32_t*)(0x40020008))
+#define DMA_CNDTR1 (*(volatile uint32_t*)(0x4002000C))
 #define DMA_CPAR1 (*(volatile uint32_t*)(0x40020010))
 #define DMA_CMAR1 (*(volatile uint32_t*)(0x40020014))
 
-#define DMA_CCR2 (*(volatile uint16_t*)(0x4002001C))
-#define DMA_CNDTR2 (*(volatile uint16_t*)(0x40020020))
+#define DMA_CCR2 (*(volatile uint32_t*)(0x4002001C))
+#define DMA_CNDTR2 (*(volatile uint32_t*)(0x40020020))
 #define DMA_CPAR2 (*(volatile uint32_t*)(0x40020024))
 #define DMA_CMAR2 (*(volatile uint32_t*)(0x40020028))
 
-#define DMA_CCR3 (*(volatile uint16_t*)(0x40020030))
-#define DMA_CNDTR3 (*(volatile uint16_t*)(0x40020034))
+#define DMA_CCR3 (*(volatile uint32_t*)(0x40020030))
+#define DMA_CNDTR3 (*(volatile uint32_t*)(0x40020034))
 #define DMA_CPAR3 (*(volatile uint32_t*)(0x40020038))
 #define DMA_CMAR3 (*(volatile uint32_t*)(0x4002003C))
 
-#define DMA_CCR4 (*(volatile uint16_t*)(0x40020044))
-#define DMA_CNDTR4 (*(volatile uint16_t*)(0x40020048))
+#define DMA_CCR4 (*(volatile uint32_t*)(0x40020044))
+#define DMA_CNDTR4 (*(volatile uint32_t*)(0x40020048))
 #define DMA_CPAR4 (*(volatile uint32_t*)(0x4002004C))
 #define DMA_CMAR4 (*(volatile uint32_t*)(0x40020050))
 
-#define DMA_CCR5 (*(volatile uint16_t*)(0x40020058))
-#define DMA_CNDTR5 (*(volatile uint16_t*)(0x4002005C))
+#define DMA_CCR5 (*(volatile uint32_t*)(0x40020058))
+#define DMA_CNDTR5 (*(volatile uint32_t*)(0x4002005C))
 #define DMA_CPAR5 (*(volatile uint32_t*)(0x40020060))
 #define DMA_CMAR5 (*(volatile uint32_t*)(0x40020064))
 
-#define DMA_CCR6 (*(volatile uint16_t*)(0x4002006C))
-#define DMA_CNDTR6 (*(volatile uint16_t*)(0x40020070))
+#define DMA_CCR6 (*(volatile uint32_t*)(0x4002006C))
+#define DMA_CNDTR6 (*(volatile uint32_t*)(0x40020070))
 #define DMA_CPAR6 (*(volatile uint32_t*)(0x40020074))
 #define DMA_CMAR6 (*(volatile uint32_t*)(0x40020078))
 
-#define DMA_CCR7 (*(volatile uint16_t*)(0x40020080))
-#define DMA_CNDTR7 (*(volatile uint16_t*)(0x40020084))
+#define DMA_CCR7 (*(volatile uint32_t*)(0x40020080))
+#define DMA_CNDTR7 (*(volatile uint32_t*)(0x40020084))
 #define DMA_CPAR7 (*(volatile uint32_t*)(0x40020088))
 #define DMA_CMAR7 (*(volatile uint32_t*)(0x4002008C))
 
